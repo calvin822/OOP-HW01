2aed8c9ec438931e2f8926a2b24d611096290294
user <user@FD08>
2016-09-30 10:02:49 +0800

    HW1

---
+++ src/fcu/iecs/oop/Hello1.java
@@ -0,0 +1,14 @@
+package fcu.iecs.oop;
+
+public class Hello1 {
+
+	public static void main(String[] args) {
+		
+		String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
+		String str2 = lyrics.replace("it", "\"her\"");
+			
+		System.out.println(str2.replace("Cannot", "Can't"));
+		
+	}
+
+}
